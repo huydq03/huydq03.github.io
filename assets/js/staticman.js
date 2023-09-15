7a90baae50e71afb00cd6debe8bc662cec50a003
Đỗ Quốc Huy <122000213+huydq03@users.noreply.github.com>
2023-09-15 10:01:16 +0700

    Update staticman.js

--- assets/js/staticman.js
+++ assets/js/staticman.js
@@ -63,3 +63,34 @@ layout: null
     }
   }
 })(jQuery);
+
+<!-- Messenger Plugin chat Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plugin chat code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+  
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "119505747803039");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    
+
+    <!-- Your SDK code -->
+    
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v18.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    
